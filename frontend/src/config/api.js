db38d93f0bda8b896b9c9bd7c0b25dc12b62b6e3
vocaotanngoc <caongoctanvo@gmail.com>
2026-07-11 02:05:02 +0700

    fix http protocol in api config
    Signed-off-by: vocaotanngoc <caongoctanvo@gmail.com>

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 const API_CONFIG = {
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://35.240.156.187:8080"
+      ? "http://35.240.156.187:8080"
       : "http://localhost:8080",
 
   // Cấu hình fetch mặc định
